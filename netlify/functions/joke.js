ea62e86e0d8f4352b44ebe1adaadb44671ba2319
codygriffith <cody@alenthea.com>
2022-06-19 07:04:48 -0400

    Update joke.js

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -1,27 +1,10 @@
 // Jokes provided from the lovely folks at https://icanhazdadjoke.com
 import jokes from './data/jokes.json'
-// const createLighthouse = require('lighthouse-lambda')
-
 
 export const handler = async () => {
   // Generates a random index based on the length of the jokes array
   const randomIndex = Math.floor(Math.random() * jokes.length)
   const randomJoke = jokes[randomIndex]
-
-  let results;
-
-  console.log('ran')
-
-  // createLighthouse('https://alenthea.com', { logLevel: 'info' })
-  //   .then(({ chrome, results }) => {
-  //         results = results;
-  //         // Do something with `results`
-  //         return chrome.kill().then(() => callback(null))
-  //       })
-  //       .catch((error) => {
-  //         // Handle errors when running Lighthouse
-  //         return chrome.kill().then(() => callback(error))
-  //       })
 
   // Netlify Functions need to return an object with a statusCode
   // Other properties such as headers or body can also be included.
